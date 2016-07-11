eceac11f804df1d40358436d52bd17169602e4f4
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2016-07-12 01:59:35 +0300

    Better ci scripts (#32)

---
+++ ci/pr.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -xe
+
+# You can run it from any directory.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_DIR=$DIR/..
+
+"$PROJECT_DIR"/gradlew -s clean build --info --no-daemon -p "$PROJECT_DIR"
